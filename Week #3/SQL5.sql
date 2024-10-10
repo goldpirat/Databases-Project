7e05e438a613a018b48b8ccf08daf92c78340d77
Flori Kusari <149197022+goldpirat@users.noreply.github.com>
2024-10-10 18:34:15 +0200

    .

--- Week #3/SQL5.sql
+++ Week #3/SQL5.sql
@@ -1,5 +1,5 @@
-SELECT B.Title, U.Name
+SELECT B.Title, U.Username
 FROM Reservation R
 JOIN Book B ON R.Book_ID = B.Book_ID
-JOIN Librarian L ON R.User_ID = L.User_ID
-JOIN User U ON L.User_ID = U.User_ID;
+JOIN User U ON R.User_ID = U.User_ID
+WHERE U.Role = 'Librarian';  -- Fetch only librarians' reservations
