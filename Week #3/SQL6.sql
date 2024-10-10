c37f7c35cbdece2dd77207fcafe39be1cb4cc15c
Flori Kusari <149197022+goldpirat@users.noreply.github.com>
2024-10-10 18:34:39 +0200

    .

--- Week #3/SQL6.sql
+++ Week #3/SQL6.sql
@@ -1,9 +1,2 @@
-SELECT U.Name, 'Librarian' AS Role
-FROM User U
-JOIN Librarian L ON U.User_ID = L.User_ID
-
-UNION
-
-SELECT U.Name, 'Regular User' AS Role
-FROM User U
-JOIN Regular_User RU ON U.User_ID = RU.User_ID;
+SELECT U.Username, U.Role
+FROM User U;
